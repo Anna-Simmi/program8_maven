624b7404ee69f811b0eba388854074189bfe60ea
Anna-Simmi <annasimmim@gmail.com>
2025-05-28 23:10:58 +0530

    first commit

---
+++ src/main/java/com/prg5/prg5/App.java
@@ -0,0 +1,13 @@
+package com.prg5.prg5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
